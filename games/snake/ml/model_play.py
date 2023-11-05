0c0957f625b1f44fcc079a856515cd9bdfdce800
Study_boy <junee2014@hotmail.com>
2023-11-05 15:15:13 +0800

    Play with model

--- games/snake/ml/model_play.py
+++ games/snake/ml/model_play.py
@@ -1,25 +1,21 @@
-"""
-The template of the script for playing the game in the ml mode
-"""
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import os
 import random
 import numpy as np
 from collections import deque
 from enum import Enum
-# from game import SnakeGameAI, Direction, Point
 from .model import Linear_QNet, QTrainer
 from .helper import plot
-# import agent
 from collections import namedtuple
-import pygame
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
 LR = 0.001
 SCENE_WIDTH = 300
 SCENE_HEIGHT = 300
-
-Point = namedtuple('Point', 'x, y')
 
 class Direction(Enum):
     RIGHT = 1
@@ -36,17 +32,16 @@ class MLPlay:
         self.epsilon = 0 # randomness
         self.gamma = 0.6 # discount rate
         self.memory = deque(maxlen=MAX_MEMORY) # popleft()
-        # self.model = Linear_QNet(11, 256, 3)
-        self.trainer = QTrainer(Linear_QNet(11, 256, 3), lr=LR, gamma=self.gamma)
+        self.model = Linear_QNet(11, 256, 3)
+        self.model.load_state_dict(torch.load("model/best_model.pth"))
+        # for param in self.model.parameters():
+        #     print(param)
+        # self.trainer = QTrainer(self.model, lr=LR, gamma=self.gamma)
         self.state_old = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.direction = Direction.DOWN
         self.highest_score = 0
         self.previous_food = (0,0)
         self.final_move_old = [1,0,0]
-        self.total_score = 0
-        self.plot_scores = []
-        self.plot_mean_scores = []
-
 
     def update(self, scene_info):
         """
@@ -56,46 +51,22 @@ class MLPlay:
         self.snake_head = scene_info["snake_head"]
         self.food = scene_info["food"]
         self.snake_body = scene_info['snake_body']
-        score = len(self.snake_body) + 1 -4
-        reward = 0
-        
-        if self.snake_head == self.previous_food:
-            reward = 10
-        if scene_info["status"] == "GAME_OVER":
-            print("game over")
-            reward = -10
 
         # get old state
         state_new = self.get_state()
-        
+
         # get move
         final_move = self.get_action(state_new)
         # print(final_move,self.direction)
         
-        # train short memory
-        self.train_short_memory(self.state_old, self.final_move_old, reward, state_new, False if scene_info["status"] != "GAME_OVER" else True)
-
-        # remember
-        self.remember(self.state_old, self.final_move_old, reward, state_new, False if scene_info["status"] != "GAME_OVER" else True)
-
         self.state_old = state_new
         self.previous_food = self.food
         self.final_move_old = final_move
 
         if scene_info["status"] == "GAME_OVER":
             self.n_games += 1
-            self.train_long_memory()
+            print(self.n_games,self.highest_score)
             self.direction = Direction.DOWN
-            if self.highest_score < score:
-                self.highest_score = score
-                self.trainer.model.save(f'model{self.n_games}.pth')
-                self.trainer.model.save('best_model.pth')
-            print('Games:',self.n_games,'Highest Score:',self.highest_score)
-            self.plot_scores.append(score)
-            self.total_score += score
-            mean_score = self.total_score / self.n_games
-            self.plot_mean_scores.append(mean_score)
-            plot(self.plot_scores, self.plot_mean_scores)
             return "RESET"
 
         del self.snake_head
@@ -150,10 +121,6 @@ class MLPlay:
         """
         Reset the status if needed
         """
-        self.state_old = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.direction = Direction.DOWN
-        self.previous_food = (0,0)
-        self.final_move_old = [1,0,0]
         return "RESET"
 
     def is_collision(self, pt=None):
@@ -216,35 +183,33 @@ class MLPlay:
 
         return np.array(state, dtype=int)
 
-    def remember(self, state, action, reward, next_state, done):
-        self.memory.append((state, action, reward, next_state, done)) # popleft if MAX_MEMORY is reached
-
-    def train_long_memory(self):
-        if len(self.memory) > BATCH_SIZE:
-            mini_sample = random.sample(self.memory, BATCH_SIZE) # list of tuples
-        else:
-            mini_sample = self.memory
-
-        states, actions, rewards, next_states, dones = zip(*mini_sample)
-        self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
-
-    def train_short_memory(self, state, action, reward, next_state, done):
-        self.trainer.train_step(state, action, reward, next_state, done)
-
     def get_action(self, state):
         # random moves: tradeoff exploration / exploitation
-        self.epsilon = 80 - self.n_games
-        final_move = [0,0,0]
-        if random.randint(0, 200) < self.epsilon:
-            move = random.randint(0, 2)
-            final_move[move] = 1
-        else:
+        with torch.no_grad():
+            self.model.eval()
+            final_move = [0,0,0]
             state0 = torch.tensor(state, dtype=torch.float)
-            prediction = self.trainer.model(state0)
+            prediction = self.model(state0)
             move = torch.argmax(prediction).item()
+            # if move==1:
+            #     move=2
+            # elif move==2:
+            #     move = 1
             final_move[move] = 1
+            return final_move
 
-        return final_move
+# PATH = "./model/gamma0.6hi45.pth"
+# model = Linear_QNet(11,256,3)
+# model.load_state_dict(torch.load(PATH))
+# # model = torch.load(PATH)
+# state = [0,0,0,0,1,0,0,0,1,0,1]
+# state0 = torch.tensor(state, dtype=torch.float)
+# prediction = model(state0)
+# move = torch.argmax(prediction).item()
+# print(move)
+# # model = torch.load(PATH)
+# # # model.eval()
+# # print(model)
+# # for param in model.parameters():
+# #     print(param)
 
