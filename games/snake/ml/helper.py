1ee6c5ba666deeb9abc31762c2b0050a6f826c42
Study_boy <junee2014@hotmail.com>
2023-11-05 16:06:46 +0800

    Update helper.py

--- games/snake/ml/helper.py
+++ games/snake/ml/helper.py
@@ -3,7 +3,7 @@ from IPython import display
 
 plt.ion()
 
-def plot(scores, mean_scores):
+def plot(scores, mean_scores, filename='Training Score.jpg'):
     display.clear_output(wait=True)
     display.display(plt.gcf())
     plt.clf()
@@ -17,4 +17,4 @@ def plot(scores, mean_scores):
     plt.text(len(mean_scores)-1, mean_scores[-1], str(mean_scores[-1]))
     plt.show(block=False)
     plt.pause(.1)
-    plt.savefig('Training Result.jpg')
+    plt.savefig(filename)
